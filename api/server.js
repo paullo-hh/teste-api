50b773dbbf0e63f7ef546674ff832c35e9a5a37d
Paulo H. Sousa <paulo.carvalho.1524995@sga.pucminas.br>
2025-05-23 11:32:25 -0300

    Remove dotenv and react-native-dotenv packages and their associated files
    - Deleted dotenv package and its package.json, README.md, LICENSE, and index.js files.
    - Removed react-native-dotenv package and its package.json, README.md, LICENSE, and index.js files.
    - Updated package-lock.json to reflect the removal of dotenv and react-native-dotenv.
    - Added serverless-http package to dependencies.
    - Modified vercel.json to update the serverless function configuration.

--- api/server.js
+++ api/server.js
@@ -1,20 +1,19 @@
 const jsonServer = require("json-server");
+const serverless = require("serverless-http");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
-
-
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
-
 server.use(
     jsonServer.rewriter({
         "/*": "/$1",
     })
 );
 server.use(router);
-server.listen(3000, () => {
-    console.log("Json Server is running");
-});
 
-module.exports = server;
+// server.listen(3000, () => {
+//     console.log("Json Server is running");
+// });
+
+module.exports.handler = serverless(server);
