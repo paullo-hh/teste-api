bd8a38a719f93bb3bbfec92179be1b2923199158
Paulo H. Sousa <paulo.carvalho.1524995@sga.pucminas.br>
2025-05-23 11:58:22 -0300

    fix: update server configuration and file references in api setup

--- api/index.js
+++ api/index.js
@@ -1,19 +1,10 @@
-const jsonServer = require("json-server");
-const serverless = require("serverless-http");
+const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router("db.json");
+const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
-server.use(
-    jsonServer.rewriter({
-        "/*": "/$1",
-    })
-);
+server.use(jsonServer.rewriter({ '/api/*': '/$1' }));
 server.use(router);
 
-// server.listen(3000, () => {
-//     console.log("Json Server is running");
-// });
-
-module.exports.handler = serverless(server);
+module.exports = server;
